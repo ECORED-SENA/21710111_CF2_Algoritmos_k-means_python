939df4a668d187df7de4f1abfb940ada0fdd68a5
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2023-09-04 19:20:53 -0500

    Material accesible

--- src/config/global.js
+++ src/config/global.js
@@ -107,7 +107,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/21710111_CF02_DU.zip',
       },
       {
         icono: 'fas fa-download',
